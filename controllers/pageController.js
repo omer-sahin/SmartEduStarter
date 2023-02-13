63d9b9274c13f38c149bc619475431ac990a1fa7
koddiyari.com <omers4hin04@gmail.com>
2023-02-13 22:56:55 +0300

    Nodemailer Kullanımı

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer=require("nodemailer")
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.render("index", {
@@ -20,4 +22,62 @@ exports.getLoginPage = (req, res) => {
   res.render("login", {
     pages: "login",
   });
+};
+
+
+exports.getContact = (req, res) => {
+  res.render("contact", {
+    pages: "contact",
+  });
+};
+
+
+
+exports.sendEmail =async (req, res) => {
+  const outputMessage=`
+  <h1>Message Details </h1>
+  <ul>
+  <li> Name: ${req.body.name} </li>
+  <li> Email:${req.body.email} </li>
+  </ul>
+  <h1> Message </h1>
+  <p> ${req.body.message} </p>`;
+
+  
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+   
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "sahinomer514@gmail.com", // Gmail
+        pass: "cjhasbtupnlepiym", // Gmail password
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <sahinomer514@gmail.com>', // sender address
+      to: "omer.s4hin04@gmail.com", // list of receivers
+      subject: "Smart EDU Contact Form ✔", // Subject line
+     
+      html:outputMessage, // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  
+
+
+  res.status(200).redirect("/contact")
+
+ 
 };
