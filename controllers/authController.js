d9665274fdaa434329d6bd8cc7e05bc4639cdeba
koddiyari.com <omers4hin04@gmail.com>
2023-02-14 17:46:00 +0300

    Express- Validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,28 @@ const User = require("../models/User");
 const bcyrpt = require("bcrypt");
 const Category=require("../models/Category")
 const Course=require("../models/Course")
-
+const {  validationResult } = require('express-validator');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
     res.status(201).redirect("/login");
   } catch (error) {
-    res.status(400).redirect("/login");
+
+
+
+    const errors=validationResult(req)
+
+    
+   for (let i = 0; i < errors.array().length; i++) {
+    req.flash("error",`${errors.array()[i].msg} `)
+    
+    
+   }
+
+   
+
+    res.status(400).redirect("/register");
   }
 };
 
@@ -19,13 +33,42 @@ exports.loginUser = async (req, res) => {
     await User.findOne({ email }, (err, user) => {
       if (user) {
         bcyrpt.compare(password, user.password, (err, same) => {
-        
+          
+          if(same){
+
             req.session.userID = user._id;
             res.status(200).redirect("/users/dashboard");
+          }
+          else{
+
+            req.flash("error",`Hatalı Şifre girdiniz Lütfen tekrar deneyin `)
+            res.status(200).redirect("/login");
+
+
+          }
+           
           
         });
-      } else {
-        res.status(201).redirect("/register");
+      }  else {
+      
+
+          
+
+          setTimeout(() => {
+            req.flash("error",`Böyle bir kullanıcı bulunamadı`)
+            
+          }, 500);
+
+
+          setTimeout(() => {
+            res.status(201).redirect("/register");
+            
+          }, 3000);
+
+         
+
+
+        
       }
     }).clone();
   } catch (error) {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,36 @@
 const router=require("express").Router();
 const authController=require("../controllers/authController");
 const authMiddleware=require("../middlewares/authmiddleware");
+const User=require("../models/User");
+
+const { body } = require('express-validator');
+
+
+router.route("/signup").post(
+    [
+        body("name").not().isEmpty().withMessage("Lütfen Adınızı Giriniz ..."),
 
 
 
 
-router.route("/signup").post(authController.createUser);
+        body("email").isEmail().withMessage("Lütfen bir email girin ...")
+        .custom((userEmail)=>{
+            return User.findOne({email:userEmail}).then(user=>{
+                if(user){
+                    return Promise.reject("Girilen Email mevcut ")
+                }
+            })
+        }),
+
+
+
+
+
+        body("password").not().isEmpty().withMessage("Lütfen şifre Belirleyiniz ..."),
+        body('password').isLength({ min: 8 }).withMessage(`Karakter sayısı en az 8 karakterden oluşmalı    `),
+        
+       
+    ], authController.createUser);
 router.route("/login").post(authController.loginUser)
 router.route("/logout").get(authController.logoutUser)
 router.route("/dashboard").get(authMiddleware,authController.getDashboardPage)
